48916e7437a7cf22de05317633df37250938ccdc
Reiniel Ravelo <reinielravelo@gmail.com>
2019-06-15 13:39:59 -0400

    Routiung set up for pages

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -3,6 +3,11 @@
 const fs = require('fs');
 // to create an http we server 
 const http = require('http'); 
+// to filter/routing diffrent URLS on the page
+const url = require('url');
+
+
+
 
 // to pull the data file from the data folder 
 const json = fs.readFileSync(`${__dirname}/data/data.json` , 'utf-8'); 
@@ -13,8 +18,25 @@ console.log(laptopData);
 
 // server request 
 const server = http.createServer((req, res) =>{
-    res.writeHead(404, { 'Content-type' : 'text/html'});
-    res.end('This is the response');
+
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+    // const name = url.parse(req.url, true).query.name;
+    
+    if (pathName === '/products' || pathName === '/') {
+        res.writeHead(202, { 'Content-type' : 'text/html'});
+        res.end('This is the Products page');
+    } 
+    
+    else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(202, { 'Content-type' : 'text/html'});
+        res.end(`This is the Laptop page for laptop ${id}!`);
+    } 
+
+    else {
+        res.writeHead(404, { 'Content-type' : 'text/html'});
+        res.end('URL was not found on the server!');
+    }
 
 });
 //Reg function
@@ -23,7 +45,7 @@ const server = http.createServer((req, res) =>{
 // });
 
 
-// listening to the request and assigning a local host
+// Listening to the request and assigning a local host
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests now'); 
 });
